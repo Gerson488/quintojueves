f6ecd54818a44675c243993828b023385fed8a5e
Gers <romerogerson488@gmail.com>
2025-10-10 12:45:21 -0500

    cambios realizados despues de las 12

--- proyecto03/app.js
+++ proyecto03/app.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
 const connection = require('./database/connection')
 const morgan = require('morgan')
-
 const userLogin = require('./middlewares/userLogin')
 const userRouter = require('./routers/userRouters')
 const reviewRouter = require('./routers/reviewRouters')
 const productRouter = require('./routers/productRouters') 
+const authRouter=require('./routers/authRouter')
 
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
+
+app.use(morgan('dev'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     const data = {
@@ -23,17 +25,15 @@ app.get('/', (req, res) => {
         "items": [1, 2, 3, 4, 5]
     }
     res.render('index', data);
-})
+});
 
+app.use('/auth', authRouter);
 
-app.use(userLogin)
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(userLogin);
 
-
-app.use('/users', userRouter)
-app.use('/reviews', reviewRouter)
-app.use('/products', productRouter) 
+app.use('/users', userRouter);
+app.use('/reviews', reviewRouter);
+app.use('/products', productRouter);
 
 
 connection
---
+++ proyecto03/controllers/authController.js
@@ -0,0 +1,32 @@
+const AuthService = require('../services/authService')
+const bcrypt=require('bcrypt')
+const authService = new AuthService()
+
+exports.register = async (req, res) => {
+    try {
+        await authService.register(req.body)
+        res.status(201).json({ 'message': 'Usuario del administrador resgistrado' })
+    } catch (error) {
+        res.status(500).json({ "error": error.message })
+    }
+}
+
+exports.login = async(req, res) => {
+    try{
+        const{email,password}=req.body
+        const userAuth=await authService.filterByEmail(email)
+        if(!email){
+            return res.status(400).json({"message":"Usuario del administrador no encontrado"})
+        }
+        const payload={
+            email:userAuth.email
+        }
+        const token=authService.generateToken(payload)
+        res.status(200).send(token)
+
+    }catch(error){
+        res.status(500).json({"error":error.message})
+    }
+
+
+}
